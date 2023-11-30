c7e4c67294c56ef463e3c8315f33ec82b373b9ef
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2023-11-30 15:01:57 -0800

    Add files via upload

---
+++ dinner-menu.py
@@ -0,0 +1,37 @@
+# someone create a home page, Ex: Welcome to The Dinner Menu (or some made up restaurant)
+
+
+
+# create a list of checkboxes: Appetizer, Drink, Main Course, Dessert)
+
+
+
+# --------someone create radio buttons for Appetizers (Wings, Fries, Oysters)
+
+
+
+
+
+
+# someone create radio buttons for Drink (Virgin Sangria, Virgin Mimosa, Virgin Wine, Virgin Margarita)
+
+
+
+
+# someone create radio buttons for Main Course (Bucket of Fried Chicken, Filet Mignon, Salmon)
+
+
+
+
+# someone create radio buttons for Dessert (Double Choclate Cake, Vanilla Ice Cream)
+
+
+
+# someone create a dinner layout with a table and each chosen food.
+
+
+
+# someone create a receipt
+
+
+
