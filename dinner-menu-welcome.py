e56788ff63b58588f4a0c39abc5df0f4f18907a9
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2023-11-30 15:21:17 -0800

    Add files via upload

---
+++ dinner-menu-welcome.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+
+dinnerMenu = Tk()
+dinnerMenu.geometry("450x550")
+dinnerMenu.title("Dinner Menu")
+
+welcome = Label(text="Welcome to The Dinner Place")
+welcome.pack()
+
+dinnerMenu.mainloop()
