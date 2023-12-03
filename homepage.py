85ca8639bf3b9220d52a5e5040f3274fd85aa835
JairoE27 <152563371+JairoE27@users.noreply.github.com>
2023-12-02 22:01:34 -0800

    Create homepage.py

---
+++ homepage.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+
+dinnerMenu = Tk()
+dinnerMenu.geometry("550x550")
+dinnerMenu.configure(bg="grey")
+dinnerMenu.title("Dinner Menu")
+
+
+
+welcome = Label(text="Welcome to The Dinner Spot")
+welcome.pack()
+
+welcome = Label(text="Welcome to The Dinner Spot", bg="Blue", fg="Pink", font=("Courier", 26, "italic"))
+welcome.config
+welcome.pack()
+dinnerMenu.mainloop()
